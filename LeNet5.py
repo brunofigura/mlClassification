7224850b39434a012e49d1a7cddf136b8d2bf32a
bruno <carl-bruno.figura@rsh-duesseldorf.de>
2024-05-16 18:16:06 +0200

    fixes identation

--- LeNet5.py
+++ LeNet5.py
@@ -38,23 +38,23 @@ def main():
     #Das Netz modellieren
 
     class LeNet5(nn.Module):
-    def __init__(self, num_classes):
-        super(ConvNeuralNet, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm2d(6),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        self.fc = nn.Linear(400, 120)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(120, 84)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(84, num_classes)
+        def __init__(self, num_classes):
+                super(ConvNeuralNet, self).__init__()
+                self.layer1 = nn.Sequential(
+                    nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+                    nn.BatchNorm2d(6),
+                    nn.ReLU(),
+                    nn.MaxPool2d(kernel_size = 2, stride = 2))
+                self.layer2 = nn.Sequential(
+                    nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+                    nn.BatchNorm2d(16),
+                    nn.ReLU(),
+                    nn.MaxPool2d(kernel_size = 2, stride = 2))
+                self.fc = nn.Linear(400, 120)
+                self.relu = nn.ReLU()
+                self.fc1 = nn.Linear(120, 84)
+                self.relu1 = nn.ReLU()
+                self.fc2 = nn.Linear(84, num_classes)
         
     def forward(self, x):
         out = self.layer1(x)
