6037361c78d483f782ab0feb7c5ba08fbc55f111
bruno <carl-bruno.figura@rsh-duesseldorf.de>
2024-05-17 14:50:19 +0200

    working LeNet5

--- LeNet5.py
+++ LeNet5.py
@@ -1,107 +1,104 @@
-# LeNet5 Architektur kann Input-Dimensionen von 32x32x1 annehmen. Damit ist es auf Grausstufenbilder ausgelegt mit nur einem "Helligkeits"-Kanal
-
-# Libraries importieren
 import torch
 import torch.nn as nn
-import torch.utils
-import torch.utils.data
-import torch.utils.data.sampler
+import torch.optim as optim
 import torchvision
-import torchvision.transforms as transforms 
+import torchvision.transforms as transforms
+import sklearn 
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
+# 1. Definition der LeNet-5 Architektur
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 def main():
+    # Check if CUDA is available and set device accordingly
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device: {device}')
 
-    # Hyperparamater
-    batch_size = 64
-    num_classes = 10
-    learning_rate = 0.001
-    num_epochs = 10
+    # 2. Daten laden und vorbereiten
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-    #Loss-Funktion
-    cost = nn.CrossEntropyLoss()
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
+                                              shuffle=True, num_workers=2)
 
-    
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=32,
+                                             shuffle=False, num_workers=2)
 
+    # 3. Netzwerkinstanziierung und auf das Gerät verschieben
+    model = LeNet5().to(device)
 
-    #Gerät für die Modell-Berechnung auswählen
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # 4. Loss-Funktion und Optimierer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    #Datensatz Transformation
-    transformTrain = transforms.Compose([transforms.Resize((32,32)),transforms.ToTensor(),transforms.Normalize(mean = (0.1307,), std = (0.3081,))])
-    transformTest = transforms.Compose([transforms.Resize((32,32)),transforms.ToTensor(),transforms.Normalize(mean = (0.1325,), std = (0.3105,))])
-    
-    
+    # 5. Training des Netzwerks
+    num_epochs = 5
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
-    
-    #Datensatz in die Loader laden
-    train_dataset = torchvision.datasets.MNIST(root = './data', train = True, transform = transformTrain, download = True)
-    
-    test_dataset = torchvision.datasets.MNIST(root = './data', train = True, transform = transformTest, download = True)
-    
-    #Train und Val Daten-Split
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f'Epoch {epoch+1}, Batch {i+1}, Loss: {running_loss/100:.3f}')
+                running_loss = 0.0
 
-    # this function will generate random indexes between 0 and 59999
-    def split_indices(n, val_per, seed = 0):
-        n_val = int(n * val_per)
-        np.random.seed(seed)
-        idx = np.random.permutation(n)
-        return idx[n_val : ], idx[: n_val]
+    print('Training beendet')
 
-    val_per = 0.2
-    rand_seed = 42
+    # 6. Evaluation des Netzwerks
+    net.eval()
+    test_loss = 0.0
+    correct = 0
+    all_predictions = []
+    all_labels = []
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            test_loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            all_predictions.extend(predicted.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
 
-    train_indices, val_indices = split_indices(len(train_dataset), val_per, rand_seed)
+    test_loss /= len(testloader.dataset)
+    accuracy = correct / len(testloader.dataset)
+    precision = precision_score(all_labels, all_predictions, average='macro')
+    recall = recall_score(all_labels, all_predictions, average='macro')
+    f1 = f1_score(all_labels, all_predictions, average='macro')
 
-    print(len(train_indices), len(val_indices))
-        
-    # Lets plot some indexes
-
-    print("Validation Indices: ", val_indices[:20])
-    print("Training Indices: ", train_indices[:20])
-
-
-    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_indices)
-    val_sampler = torch.utils.data.sampler.SubsetRandomSampler(val_indices)
-
-    train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, sampler = train_sampler)
-    
-    val_loader = torch.utils.data.DataLoader(dataset = test_dataset,batch_size = batch_size, sampler = val_sampler)
-
-    def show_batch(dataLoader):
-        for img, label in dataLoader:
-            fig, ax = plt.subplots(figsize = (12, 8))
-            ax.imshow(torchvision.utils.make_grid(img[:110], 10).permute(1,2,0))
-            break
-    
-    show_batch(val_loader)
-    plt.show(block=False)
-    plt.pause(1)
-    
-    ###################
-    #NETZ-MODELLIERUNG#
-    ###################
-
-   
-    
-    ##########
-    #TRAINING#
-    ##########
-
-    
-
-    
-                
-    #########
-    #TESTING#
-    #########
-
-    
-    
+    print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, Cross-Entropy Loss: {test_loss:.4f}')
 
 if __name__ == '__main__':
     main()
