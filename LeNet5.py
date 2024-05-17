7b022c880360fa9a64f6768b56de96881eb14322
bruno <carl-bruno.figura@rsh-duesseldorf.de>
2024-05-17 11:38:18 +0200

    Adds complete Model ( notworking )

--- LeNet5.py
+++ LeNet5.py
@@ -6,6 +6,10 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms 
 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
 
 def main():
 
@@ -14,6 +18,12 @@ def main():
     num_classes = 10
     learning_rate = 0.001
     num_epochs = 10
+
+    #Loss-Funktion
+    cost = nn.CrossEntropyLoss()
+
+    
+
 
     #Gerät für die Modell-Berechnung auswählen
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -34,26 +44,27 @@ def main():
     
     print(test_dataset)
     
-    
-    #Das Netz modellieren
+    ###################
+    #NETZ-MODELLIERUNG#
+    ###################
 
     class LeNet5(nn.Module):
         def __init__(self, num_classes):
-                self.layer1 = nn.Sequential(
-                    nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
-                    nn.BatchNorm2d(6),
-                    nn.ReLU(),
-                    nn.MaxPool2d(kernel_size = 2, stride = 2))
-                self.layer2 = nn.Sequential(
-                    nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
-                    nn.BatchNorm2d(16),
-                    nn.ReLU(),
-                    nn.MaxPool2d(kernel_size = 2, stride = 2))
-                self.fc = nn.Linear(400, 120)
-                self.relu = nn.ReLU()
-                self.fc1 = nn.Linear(120, 84)
-                self.relu1 = nn.ReLU()
-                self.fc2 = nn.Linear(84, num_classes)
+            self.layer1 = nn.Sequential(
+                nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+                nn.BatchNorm2d(6),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 2, stride = 2))
+            self.layer2 = nn.Sequential(
+                nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size = 2, stride = 2))
+            self.fc = nn.Linear(400, 120)
+            self.relu = nn.ReLU()
+            self.fc1 = nn.Linear(120, 84)
+            self.relu1 = nn.ReLU()
+            self.fc2 = nn.Linear(84, num_classes)
         
     def forward(self, x):
         out = self.layer1(x)
@@ -65,7 +76,59 @@ def main():
         out = self.relu1(out)
         out = self.fc2(out)
         return out
+    
+    ##########
+    #TRAINING#
+    ##########
 
+    
+
+    #gibt an wieviele Schritte noch übrig bleiben beim Training
+    total_step = len(train_loader)
+
+    model = LeNet5(num_classes).to(device)
+    #Optimierer festlegen
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader): #über die Batches iterieren 
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            #Forward pass
+            #Vorhersage über die Klasse des Bildes durch das Modell wird berechnet
+            outputs = model(images)                 
+            #Über Kreuz-Entropie wird der loss zwischen Vorhersage und Groundtruth berechnet
+            loss = cost(outputs, labels)
+                
+            # Backward and optimize
+            # Gradienten zurücksetzen nach jedem Batch
+            optimizer.zero_grad()
+            # Gradienten mithilfe berechnetem loss neu berechnen
+            loss.backward()
+            #Gewichte des Netztes updaten mithilfe des neuen Gradienten
+            optimizer.step()
+                    
+            if (i+1) % 400 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                            .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                
+    #########
+    #TESTING#
+    #########
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+	 
 
 if __name__ == '__main__':
     main()
