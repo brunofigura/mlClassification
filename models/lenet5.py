ce711fcf501b4dba8a838b1b1c154e4feb6e19aa
bruno <carl-bruno.figura@rsh-duesseldorf.de>
2024-05-19 12:58:42 +0200

    adds and optimizes model for mnist

--- models/lenet5.py
+++ models/lenet5.py
@@ -39,3 +39,22 @@ class LeNet5(nn.Module):
         x = nn.functional.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+class LeNet5MNIST(nn.Module):
+   def __init__(self):
+        super(LeNet5MNIST, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5) #passt in Channels an, da MNIST Graustufenbilder sind
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+   def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
