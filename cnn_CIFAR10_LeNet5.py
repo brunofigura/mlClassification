25a84ab8898ccd0be87c4f012ae00f4e09b29d70
bruno <carl-bruno.figura@rsh-duesseldorf.de>
2024-06-03 12:32:39 +0200

    cnn_CIFAR10 - refactors code

--- cnn_CIFAR10_LeNet5.py
+++ cnn_CIFAR10_LeNet5.py
@@ -1,206 +1,273 @@
 import os
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-import torch.backends.cudnn as cudnn
-import sklearn 
 from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def main():
+class CNN_CIFAR(nn.Module):     #VGG architecture
+    def __init__(self, num_classes=10):
+        super(CNN_CIFAR, self).__init__()
+        self.features = nn.Sequential(
+            # Conv Block 1
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-    #Hyperparameter 
-    modelSavePath = './trainedModels/VGGCIFAR10.ckpt'
-    TRAIN_MODEL = True  # True, wenn das Modell trainiert werden soll, False, wenn es geladen werden soll
+            # Conv Block 2
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-    NUM_EPOCHS = 50
-    INIT_LR = 0.0001
-    BATCH_SIZE = 512
+            # Conv Block 3
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-    # Check if CUDA is available and set device accordingly
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(f'Using device: {device}')
+            # Conv Block 4
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-    # 2. Daten laden und vorbereiten
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)) # CIFAR Normalisation und Standartabweichung der Kanäle
-    ])
+            # Conv Block 5
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, BATCH_SIZE,
-                                              shuffle=True, num_workers=2)
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 1 * 1, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            nn.Linear(4096, num_classes),
+        )
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, BATCH_SIZE,
-                                             shuffle=False, num_workers=2)
-
-    # 3. Netzwerk definieren und auf das Gerät verschieben
-    class VGG16(nn.Module):
-        def __init__(self, num_classes=10):
-            super(VGG16, self).__init__()
-            self.features = nn.Sequential(
-                # Conv Block 1
-                nn.Conv2d(3, 64, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(64, 64, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                # Conv Block 2
-                nn.Conv2d(64, 128, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(128, 128, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                # Conv Block 3
-                nn.Conv2d(128, 256, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                # Conv Block 4
-                nn.Conv2d(256, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-
-                # Conv Block 5
-                nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-            )
-
-            self.classifier = nn.Sequential(
-                nn.Linear(512 * 1 * 1, 4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(0.2),
-                nn.Linear(4096, 4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(0.2),
-                nn.Linear(4096, num_classes),
-            )
-
-        def forward(self, x):
-            x = self.features(x)
-            x = x.view(x.size(0), -1)
-            x = self.classifier(x)
-            return x
-
-    print('==> Building model ..')
-    model = VGG16()
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
     
-    if not TRAIN_MODEL:
-        print("loading wheigts ...")
-        model.load_state_dict(torch.load(modelSavePath))
-        model = model.to(device)
-        criterion = nn.CrossEntropyLoss()
-    else:
-        model = model.to(device)
-        # 4. Loss-Funktion und Optimierer
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), INIT_LR)
+class CNN_CIFAR_Classifier:
+    def __init__(self, n_epochs, init_lr):
+        
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        torch.backends.cudnn.benchmark = False # wirft sonst Fehlermeldungen auf -> hilft auch nicht aber der Code läuft 
 
-        # 5. Training des Netzwerks
-        for epoch in range(NUM_EPOCHS):
-            model.train()
-            running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
+        self.batch_size = 64
+        self.valid_ratio = 0.1
 
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-                running_loss += loss.item()
-                if i % 10 == 9:
-                    print(f'Epoch {epoch+1}, Batch {i+1}, Loss: {running_loss/10:.3f}')
-                    running_loss = 0.0
+        self.n_epochs = n_epochs
+        self.init_lr = init_lr
+        self.img_res = 32 * 32 * 3
 
-        print('Training beendet')
+        transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
-        # Speichere das trainierte Modell
-        #os.makedirs(os.path.dirname(modelSavePath), exist_ok=True)
-       #torch.save(model.state_dict(), modelSavePath)
-        #print(f'Modell wurde unter {modelSavePath} gespeichert.')
 
-    # 6. Evaluation des Netzwerks
-    model.eval()
-    test_loss = 0.0
-    correct = 0
-    all_predictions = []
-    all_labels = []
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            test_loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
-            all_predictions.extend(predicted.cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
 
-    test_loss /= len(testloader.dataset)
-    accuracy = correct / len(testloader.dataset)
-    precision = precision_score(all_labels, all_predictions, average='macro')
-    recall = recall_score(all_labels, all_predictions, average='macro')
-    f1 = f1_score(all_labels, all_predictions, average='macro')
+        self.train_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                train=True,
+                                download=True,
+                                transform=transform)
 
-    print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, Cross-Entropy Loss: {test_loss:.4f}')
+        self.test_dataset = torchvision.datasets.CIFAR10(root='./data',
+                            train=False,
+                            download=True,
+                            transform=transform)
+        
+        #Validierungs-Set vom Trainingsdatensatz erzeugen
+        num_train = len(self.train_dataset)
+        num_valid = int(self.valid_ratio * num_train)
+        num_train = num_train - num_valid
 
-   # Erstelle Confusion Matrix
-    conf_matrix = confusion_matrix(all_labels, all_predictions)
+        self.train_subset, self.valid_subset = torch.utils.data.random_split(self.train_dataset, [num_train, num_valid])
+            
+        self.train_loader = torch.utils.data.DataLoader(self.train_subset,
+                                    shuffle=True,
+                                    batch_size=self.batch_size)
+        
+        self.valid_loader = torch.utils.data.DataLoader(self.valid_subset, shuffle=True, batch_size=self.batch_size)
 
-    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+
+        self.test_loader = torch.utils.data.DataLoader(self.test_dataset,
+                                    batch_size=self.batch_size)
+        
+
+
+
+        self.network = CNN_CIFAR()
+        self.network = self.network.to(self.device)
+
+        self.optimizer = optim.Adam(self.network.parameters(), self.init_lr)
+        self.criterion = nn.CrossEntropyLoss()  # Use CrossEntropyLoss instead of NLLLoss
+        
+        self.train_losses = []
+        self.train_counter = []
+        self.valid_losses = []
+        self.valid_counter = []
+        self.test_losses = []
+        self.test_counter = [i * len(self.train_loader.dataset) for i in range(self.n_epochs + 1)]
+
+        
+        total_params = sum(p.numel() for p in self.network.parameters() if p.requires_grad)
+        print(f'Total number of trainable parameters: {total_params}')
+
+    def train(self, epoch, log_interval):
+        self.network.train()
+        for batch_idx, (images, labels) in enumerate(self.train_loader):
+            images, labels = images.to(self.device), labels.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.network(images)
+            loss = self.criterion(output, labels)
+            loss.backward()
+            self.optimizer.step()
+            if batch_idx % log_interval == 0:
+                print(f'Train Epoch: {epoch} '
+                      f'[{batch_idx * len(images)}/{len(self.train_loader.dataset)} '
+                      f'({100. * batch_idx / len(self.train_loader):.0f}%)]  Loss: {loss.item():.6f}', end='\r')
+                self.train_losses.append(loss.item())
+                self.train_counter.append(
+                    (batch_idx * self.batch_size) + ((epoch - 1) * len(self.train_loader.dataset)))
+            
+
+    def validate(self):
+        self.network.eval()
+        valid_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for images, labels in self.valid_loader:
+                images, labels = images.to(self.device), labels.to(self.device)
+                output = self.network(images)
+                valid_loss += self.criterion(output, labels).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(labels.data.view_as(pred)).sum().item()
+            valid_loss /= len(self.valid_loader)
+            self.valid_losses.append(valid_loss)
+            print(f'\nValidation set: Avg. loss: {valid_loss:.4f}, '
+              f'Accuracy: {correct}/{len(self.valid_loader.dataset)}'
+              f'({100. * correct / len(self.valid_loader.dataset):.0f}%)\n')
+
+    def test(self):
+        self.network.eval()
+        test_loss = 0
+        correct = 0
+        all_predictions = []
+        all_labels = []
+        with torch.no_grad():
+            for images, labels in self.test_loader:
+                images, labels = images.to(self.device), labels.to(self.device)
+                output = self.network(images)
+                test_loss += self.criterion(output, labels).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(labels.data.view_as(pred)).sum().item()
+                all_predictions.extend(pred.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+
+        test_loss /= len(self.test_loader)
+        self.test_losses.append(test_loss)
+        accuracy = correct / len(self.test_loader.dataset)
+        precision = precision_score(all_labels, all_predictions, average='macro')
+        recall = recall_score(all_labels, all_predictions, average='macro')
+        f1 = f1_score(all_labels, all_predictions, average='macro')
+        print(f'\nTest set: Avg. loss: {test_loss:.4f}, '
+              f'Accuracy: {correct}/{len(self.test_loader.dataset)}'
+              f'({100. * correct / len(self.test_loader.dataset):.0f}%)\n'
+              f'Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.04f}')
+        
+        self.conf_matrix = confusion_matrix(all_labels, all_predictions)
+
+        
+    def plot_val_train_losses(self):
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.train_losses, label='Training Loss', color='blue')
+        plt.plot(self.valid_losses, label='Validation Loss', color='orange')
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Losses')
+        plt.legend()
+        plt.show()
+
+    def plot_confMatrix(self):
+        class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
+        
+        hyperparameters = {
+            'Epochs' : self.n_epochs,
+            'Initial Learn-Rate' : self.init_lr ,
+            'Batch Size' : self.batch_size
+        }
+        # Konvertiere die Hyperparameter in einen lesbaren String
+        hyperparameters_str = '\n'.join([f'{key}: {value}' for key, value in hyperparameters.items()])
 
-    # Definiere die Werte der Hyperparameter
-    hyperparameters = {
-        'NUM_EPOCHS': NUM_EPOCHS,
-        'INIT_LR': INIT_LR,
-        'BATCH_SIZE': BATCH_SIZE
-    }
+        # Plot Confusion Matrix
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(self.conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False, 
+                    xticklabels=class_names, yticklabels=class_names)
+        plt.title('CNN - VGG')
+        plt.xlabel('Predicted Labels')
+        plt.ylabel('True Labels')
 
-# Konvertiere die Hyperparameter in einen lesbaren String
-    hyperparameters_str = '\n'.join([f'{key}: {value}' for key, value in hyperparameters.items()])
+        # Füge eine Anmerkung mit den Hyperparametern hinzu
+        plt.annotate(hyperparameters_str, xy=(0.5, 1.05), xytext=(0.5, 1.1),
+                xycoords='axes fraction', textcoords='axes fraction',
+                fontsize=7, ha='center', va='center', bbox=dict(facecolor='none', edgecolor='black', boxstyle='round,pad=0.5'))
 
-    # Plot Confusion Matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False, 
-                xticklabels=class_names, yticklabels=class_names)
-    plt.title('LeNet5')
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('True Labels')
+        plt.xticks(rotation=45)  # Drehen Sie die Achsenbeschriftungen für bessere Lesbarkeit
+        plt.yticks(rotation=45)
+        plt.savefig('./confusion_Matrices/CIFAR_Cnn_Classifier.png')  # Speichern Sie die Confusion Matrix als PNG-Datei
+        plt.show()
 
-    # Füge eine Anmerkung mit den Hyperparametern hinzu
-    plt.annotate(hyperparameters_str, xy=(0.5, 1.05), xytext=(0.5, 1.1),
-            xycoords='axes fraction', textcoords='axes fraction',
-            fontsize=7, ha='center', va='center', bbox=dict(facecolor='none', edgecolor='black', boxstyle='round,pad=0.5'))
+    def saveModelWheights(self):
+        modelSavePath = './trainedModels/cnn_cifar.ckpt'
+        os.makedirs(os.path.dirname(modelSavePath), exist_ok=True)
+        torch.save(self.network.state_dict(), modelSavePath)
+        print(f'Modell wurde unter {modelSavePath} gespeichert.')
 
-    plt.xticks(rotation=45)  # Drehen Sie die Achsenbeschriftungen für bessere Lesbarkeit
-    plt.yticks(rotation=45)
-    plt.savefig('./confusion_Matrices/cm_CIFAR_LeNet5.png')  # Speichern Sie die Confusion Matrix als PNG-Datei
-    plt.show()
 
+
+def main():
+    n_epochs = 50
+    log_interval = 10
+    init_lr = 0.0001
+    cl = CNN_CIFAR_Classifier(n_epochs, init_lr)
+    cl.test()
+
+    for epoch in range(1, n_epochs + 1):
+        cl.train(epoch, log_interval)
+        cl.validate()
+
+    cl.plot_val_train_losses()
+
+    cl.test()
+    cl.plot_confMatrix()
+
+    cl.saveModelWheights()
 
 if __name__ == '__main__':
     main()
